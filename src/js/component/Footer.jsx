a76d3e8503f52722b99e668dfa1593b734409e8d
Alejandro <78373978+C20ALEX00@users.noreply.github.com>
2024-10-21 22:16:03 +0000

    first commit

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+
+
+const Footer = () => {
+
+
+    return (<div id="base"   class="container">
+        <footer class="py-3 my-4">
+          <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Abaut</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Servicies</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Contact</a></li>
+          </ul>
+          <p class="text-center text-body-secondary">Copyright © 2024 Company TuPáginaDeVideoJuegos, Inc 2024</p>
+        </footer>
+      </div>
+    )
+}
+
+export default Footer
---
+++ src/js/component/Jumbotron.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+
+const Jumbotron = () => {
+
+
+    return (
+        <div className="jumbotron">
+  <h1 className="display-4">Bienvenido amante de los Videojuegos</h1>
+  <p className="lead">Esta página te hace descuchir los mejores Videojuegos para todas las plataformas</p>
+  
+  <p>A continuación te muestro algunos de los mejores juegos, tanto wue hasta tienen un GOTY</p>
+  <p className="lead">
+    <a className="btn btn-primary btn-lg" href="#" role="button">Leer mas</a>
+  </p>
+</div>
+    )
+}
+
+export default Jumbotron
